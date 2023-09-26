b77c33da433eee8743f1b9751c3015a844e36163
MagedKhaled <32371968+MagedKhaled@users.noreply.github.com>
2023-09-26 16:26:36 +0300

    Update to work in other devices app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import requests
 #*****************set server*****************
 homeDir = './'
-homeDir = '/home/maged_khaled/workSpace/medadA/website/'
+#homeDir = '/home/maged_khaled/workSpace/medadA/website/'
 app = Flask(__name__)
 
 GOOGLE_CLIENT_ID = "361389956894-fikf8t93743htich35qalbib61kotq1q.apps.googleusercontent.com"
